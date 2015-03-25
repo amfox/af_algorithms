8b1e2ffb5a339c5408d175c39d40a964ad4a68db
amfox <779377798@qq.com>
2015-03-25 19:53:11 +0800

    additon vector al function
    addtion vector al function

---
+++ vector_addition_recursive.py
@@ -0,0 +1,19 @@
+# The authors of this work have released all rights to it and placed it
+# in the public domain under the Creative Commons CC0 1.0 waiver
+# (http://creativecommons.org/publicdomain/zero/1.0/).
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+# CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+# TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+# SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+# 
+# Retrieved from: http://en.literateprograms.org/Adding_vectors_(Python)?oldid=19478
+
+def vadd(vector1, vector2):
+    if vector1 and vector2:
+        return [vector1[0] + vector2[0]] + vadd(vector1[1:], vector2[1:])
+    else:
+        return []
